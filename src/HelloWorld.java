75189b4fcb6d9b604195de9702aa6fc2e1c8b454
Pierre Irby <stoked49@yahoo.com>
2017-01-03 12:19:11 -0600

    2nd Java Commit!

---
+++ src/HelloWorld.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Irby on 1/3/17.
+ */
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Howdy, world!");
+        System.out.println("All java code lives inside of classes");
+        System.out.println("Our program runs the 'main' method when executed");
+        System.out.println("Goodbye, world!");''
+    }
+}
