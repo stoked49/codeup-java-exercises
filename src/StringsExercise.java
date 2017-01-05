91c0e908680d0a48ca95423f76a4c05474c80dcd
Pierre Irby <stoked49@yahoo.com>
2017-01-05 09:51:15 -0600

    5Jan17 Java Lessons commit.

---
+++ src/StringsExercise.java
@@ -0,0 +1,18 @@
+/**
+ * Created by Irby on 1/4/17.
+ */
+public class StringsExercise {
+    public static void main (String[] args) {
+        String message1 = "We don't need no education \nWe don't need no thought control";
+        System.out.println(message1);
+
+        String message2 = "Check \"this\" out!, \"s inside of \"s!";
+        System.out.println(message2);
+
+        String message3 = "In windows, the main drive is usually C:\\";
+        System.out.println(message3);
+
+        String message4 = "I can do back slashes \\, and double back slashes \\\\, and the amazing triple back-slash \\\\\\!";
+        System.out.println(message4);
+    }
+}
